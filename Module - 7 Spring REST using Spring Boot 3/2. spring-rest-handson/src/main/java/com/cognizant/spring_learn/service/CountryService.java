1bf55f8882e1a18542cb3e613a15d25ebf935eb6
Santhoshi Matha <santhoshimatha.cse@gmail.com>
2026-07-16 17:55:41 +0530

    Completed 2. spring-rest-handson

---
+++ Module - 7 Spring REST using Spring Boot 3/2. spring-rest-handson/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,34 @@
+package com.cognizant.springlearn.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+
+@Service
+public class CountryService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryService.class);
+
+    @SuppressWarnings("unchecked")
+    public Country getCountry(String code) throws CountryNotFoundException {
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        List<Country> countries = context.getBean("countryList", ArrayList.class);
+        
+        Country country = countries.stream()
+                .filter(c -> c.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElseThrow(() -> new CountryNotFoundException());
+        
+        LOGGER.info("END");
+        return country;
+    }
+}
