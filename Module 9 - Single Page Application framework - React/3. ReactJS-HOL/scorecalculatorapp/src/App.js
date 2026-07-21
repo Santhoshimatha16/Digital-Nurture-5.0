817f5bebe6f4a43fcc143c3db76ce90727cd2579
Santhoshi Matha <santhoshimatha.cse@gmail.com>
2026-07-21 23:18:18 +0530

    Completed 4.ReactJS-HOL

---
+++ Module 9 - Single Page Application framework - React/3. ReactJS-HOL/scorecalculatorapp/src/App.js
@@ -0,0 +1,22 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <CalculateScore
+        name="Santhoshi Matha"
+        school="SKCET"
+        total={450}
+        goal={5}
+      />
+
+    </div>
+
+  );
+
+}
+
+export default App;
